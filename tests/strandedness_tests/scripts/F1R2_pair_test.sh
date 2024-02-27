015b90525a8035024f4b41201cd568c61014dedb
Eric Kofman <ekofman@tscc-11-59.tscc.sdsc.edu>
2024-02-27 09:56:40 -0800

    tests

---
+++ tests/strandedness_tests/scripts/F1R2_pair_test.sh
@@ -0,0 +1,18 @@
+python \
+/tscc/projects/ps-yeolab3/ekofman/sailor2/marine.py \
+--bam_filepath \
+/tscc/projects/ps-yeolab3/ekofman/sailor2/tests/strandedness_tests/bams/F1R2_pair.bam \
+--annotation_bedfile_path \
+/tscc/projects/ps-yeolab3/ekofman/sailor2/annotations/hg38_gencode.v35.annotation.genes.bed \
+--output_folder \
+/tscc/projects/ps-yeolab3/ekofman/sailor2/tests/strandedness_tests/F1R2_pair_test \
+--min_dist_from_end \
+0 \
+--min_base_quality \
+0 \
+--cores \
+1 \
+--paired_end \
+--contigs "chr17" \
+--sailor \
+--verbose 
