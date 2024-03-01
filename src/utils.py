86d7627245e35019241db43e3760d901b15107c7
Eric Kofman <ekofman@tscc-1-9.tscc.sdsc.edu>
2024-03-01 08:45:56 -0800

    Dont check contig ids

--- src/utils.py
+++ src/utils.py
@@ -36,13 +36,14 @@ def make_edit_finding_jobs(bampath, output_folder, reverse_stranded=True, barcod
     
     samfile = pysam.AlignmentFile(bampath, "rb")
     contig_lengths_dict = get_contig_lengths_dict(samfile)
+    print('contig_lengths_dict:{}'.format(contig_lengths_dict))
     if len(contigs) == 0:
         contigs_to_use = set(contig_lengths_dict.keys())
     else:
         contigs_to_use = set(contigs)
     for contig in contig_lengths_dict.keys():
         # Skip useless contigs
-        if len(contig) > 5 or contig == 'Stamp':# or contig != '17':
+        if contig == 'Stamp':# or contig != '17':
             continue
             
         if contig not in contigs_to_use:
@@ -52,7 +53,7 @@ def make_edit_finding_jobs(bampath, output_folder, reverse_stranded=True, barcod
         
         contig_length = contig_lengths_dict.get(contig)
         intervals_for_contig = get_intervals(contig, contig_lengths_dict, num_intervals_per_contig)
-
+        print('\t\tintervals_for_contig: {}'.format(intervals_for_contig))
         # Set up for pool
         for split_index, interval in enumerate(intervals_for_contig):
             split_index = str(split_index).zfill(3)
