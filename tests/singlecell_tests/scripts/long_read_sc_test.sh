f885d85861596b9692d0f22d0ac0ef1604013b73
Eric Kofman <ekofman@tscc-4-41.tscc.sdsc.edu>
2024-07-02 18:07:45 -0700

    test fix

--- tests/singlecell_tests/scripts/long_read_sc_test.sh
+++ tests/singlecell_tests/scripts/long_read_sc_test.sh
@@ -2,6 +2,8 @@ python \
 /tscc/projects/ps-yeolab3/ekofman/sailor2/marine.py \
 --bam_filepath \
 /tscc/projects/ps-yeolab3/ekofman/sailor2/examples/data/LR_single_cell.md.subset.filtered.sorted.bam \
+--annotation_bedfile_path \
+/tscc/projects/ps-yeolab3/ekofman/sailor2/annotations/hg38_gencode.v35.annotation.genes.bed \
 --output_folder \
 /tscc/projects/ps-yeolab3/ekofman/sailor2/tests/singlecell_tests/long_read_sc_test \
 --min_dist_from_end \
