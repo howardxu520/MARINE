99a882b1bdeab121e4e93407f6bca55fe1240596
Eric Kofman <ekofman@tscc-1-1.tscc.sdsc.edu>
2024-07-08 11:09:48 -0700

    add test

---
+++ tests/strandedness_tests/scripts/F1R2_pair_test-single_end_mode.sh
@@ -0,0 +1,18 @@
+marine.py \
+--bam_filepath \
+$MARINE/tests/strandedness_tests/bams/F1R2_pair.bam \
+--annotation_bedfile_path \
+$MARINE/annotations/hg38_gencode.v35.annotation.genes.bed \
+--output_folder \
+$MARINE/tests/strandedness_tests/F1R2_pair_test-single_end_mode \
+--min_dist_from_end \
+0 \
+--min_base_quality \
+0 \
+--cores \
+16 \
+--strandedness 2 \
+--contigs "chr17" \
+--sailor \
+--verbose \
+--num_intervals_per_contig 16
