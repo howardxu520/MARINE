a7c97250a135c656fc70ff047103027a074e36ca
Eric Kofman <ekofman@tscc-8-35.tscc.sdsc.edu>
2024-07-15 16:16:27 -0700

    enable auto-generation of bedgraphs

--- examples/test_bulk_subset_CT.sh
+++ examples/test_bulk_subset_CT.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-marine.py --bam_filepath $MARINE/examples/data/bulk_CT.md.subset.bam --output_folder $MARINE/examples/bulk_subset_CT --strandedness 2 --cores 16 --annotation_bedfile_path $MARINE/annotations/hg38_gencode.v35.annotation.genes.bed --contigs "1"
+marine.py --bam_filepath $MARINE/examples/data/bulk_CT.md.subset.bam --output_folder $MARINE/examples/bulk_subset_CT --strandedness 2 --cores 16 --annotation_bedfile_path $MARINE/annotations/hg38_gencode.v35.annotation.genes.bed --contigs "1" --bedgraphs "CT"
