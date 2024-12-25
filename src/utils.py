5b0d874eaebfaeb5ef1e3e3a31d2b70d3e82de45
Eric Kofman <ekofman@tscc-1-27.tscc.sdsc.edu>
2024-12-24 17:57:08 -0800

    sort cols and index

--- src/utils.py
+++ src/utils.py
@@ -1097,6 +1097,9 @@ def combine_coverage_adatas(adata_dict):
         dense_adata = dense_adata.join(missing_position_fix, how='inner')
         assert(len(dense_adata.columns) == len(all_pos))
 
+        dense_adata.columns = sorted(dense_adata.columns)
+        dense_adata = dense_adata.sort_index()
+        
         # Convert the dense matrix to a sparse CSR matrix
         sparse_X = sp.csr_matrix(dense_adata)
         # Create a new AnnData object with the sparse matrix
